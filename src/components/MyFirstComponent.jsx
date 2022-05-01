d55f5767e44e05ff15f364722a73be644643ac8b
Erick Valdez <erickvrz.92@gmail.com>
2022-05-01 14:06:24 -0500

    clase 1 react

---
+++ src/components/MyFirstComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MyFirstComponent = () => {
+  return (
+    <>
+    <button>Hola</button>
+    <h2>Mundo</h2>
+    </>
+  );
+};
+
+export default MyFirstComponent;
